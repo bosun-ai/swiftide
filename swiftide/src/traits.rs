58fbb9e0deb2cddbd006be173f653f08299fc07c
Timon Vonk <timonv@gmail.com>
2024-06-12 19:14:32 +0200

    feat: simple unit test for ingestion pipeline

--- swiftide/src/traits.rs
+++ swiftide/src/traits.rs
@@ -4,12 +4,18 @@ use crate::{ingestion::IngestionNode, ingestion::IngestionStream, Embeddings};
 use anyhow::Result;
 use async_trait::async_trait;
 
+/// All traits are easilly mockable under tests
+#[cfg(test)]
+use mockall::{automock, predicate::*};
+
+#[cfg_attr(test, automock)]
 #[async_trait]
 /// Transforms single nodes into single nodes
 pub trait Transformer: Send + Sync + Debug {
     async fn transform_node(&self, node: IngestionNode) -> Result<IngestionNode>;
 }
 
+#[cfg_attr(test, automock)]
 #[async_trait]
 /// Transforms batched single nodes into streams of nodes
 pub trait BatchableTransformer: Send + Sync + Debug {
@@ -20,16 +26,19 @@ pub trait BatchableTransformer: Send + Sync + Debug {
 }
 
 /// Starting point of a stream
+#[cfg_attr(test, automock)]
 pub trait Loader {
     fn into_stream(self) -> IngestionStream;
 }
 
+#[cfg_attr(test, automock)]
 #[async_trait]
 /// Turns one node into many nodes
 pub trait ChunkerTransformer: Send + Sync + Debug {
     async fn transform_node(&self, node: IngestionNode) -> IngestionStream;
 }
 
+#[cfg_attr(test, automock)]
 #[async_trait]
 /// Caches nodes, typically by their path and hash
 /// Recommended to namespace on the storage
@@ -40,17 +49,20 @@ pub trait NodeCache: Send + Sync + Debug {
     async fn set(&self, node: &IngestionNode);
 }
 
+#[cfg_attr(test, automock)]
 #[async_trait]
 pub trait Embed: Debug + Send + Sync {
     async fn embed(&self, input: Vec<String>) -> Result<Embeddings>;
 }
 
+#[cfg_attr(test, automock)]
 #[async_trait]
 pub trait SimplePrompt: Debug + Send + Sync {
     // Takes a simple prompt, prompts the llm and returns the response
     async fn prompt(&self, prompt: &str) -> Result<String>;
 }
 
+#[cfg_attr(test, automock)]
 #[async_trait]
 /// Persists nodes
 pub trait Storage: Send + Sync {
