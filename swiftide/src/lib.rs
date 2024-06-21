dbae74c5fc2b35720e713f718c24216aa39f5c61
Timon Vonk <timonv@gmail.com>
2024-06-21 14:57:19 +0200

    refactor: rename embeddings.rs to type_aliases.rs

--- swiftide/src/lib.rs
+++ swiftide/src/lib.rs
@@ -1,9 +1,9 @@
-pub mod embeddings;
 pub mod ingestion;
 pub mod integrations;
 pub mod loaders;
 pub mod traits;
 pub mod transformers;
+pub mod type_aliases;
 
-pub use embeddings::*;
 pub use traits::*;
+pub use type_aliases::*;
