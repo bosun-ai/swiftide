0fcc4eb850441398239018f15cc8a60cf1652b36
github-action-benchmark <github@users.noreply.github.com>
2024-06-30 22:19:19 +0000

    add Rust Benchmark (cargo) benchmark result for a12cce230032eebe2f7ff1aa9cdc85b8fc200eb1

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1719784899288,
+  "lastUpdate": 1719785959178,
   "repoUrl": "https://github.com/bosun-ai/swiftide",
   "entries": {
     "Rust Benchmark": [
@@ -125,6 +125,48 @@ window.BENCHMARK_DATA = {
             "name": "run_local_pipeline",
             "value": 837,
             "range": "± 2",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "timonv@gmail.com",
+            "name": "Timon Vonk",
+            "username": "timonv"
+          },
+          "committer": {
+            "email": "mail@timonv.nl",
+            "name": "Timon Vonk",
+            "username": "timonv"
+          },
+          "distinct": true,
+          "id": "a12cce230032eebe2f7ff1aa9cdc85b8fc200eb1",
+          "message": "fix(openai): add tests for builder",
+          "timestamp": "2024-07-01T00:15:11+02:00",
+          "tree_id": "4d51f3f7e0532cecac98f8a82c9c4946e8e3c487",
+          "url": "https://github.com/bosun-ai/swiftide/commit/a12cce230032eebe2f7ff1aa9cdc85b8fc200eb1"
+        },
+        "date": 1719785958253,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "load_1",
+            "value": 6,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "load_10",
+            "value": 6,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "run_local_pipeline",
+            "value": 836,
+            "range": "± 1",
             "unit": "ns/iter"
           }
         ]
