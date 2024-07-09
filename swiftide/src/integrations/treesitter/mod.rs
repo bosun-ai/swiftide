92eb2e6f8c2d146d3c1ccadf7d4a938ee1486047
Tinco Andringa <mail@tinco.nl>
2024-07-10 01:58:57 +0200

    start on tree sitter summarizer

--- swiftide/src/integrations/treesitter/mod.rs
+++ swiftide/src/integrations/treesitter/mod.rs
@@ -1,6 +1,8 @@
 //! Chunking code with tree-sitter
 mod splitter;
+mod summarizer;
 mod supported_languages;
 
 pub use splitter::{ChunkSize, CodeSplitter, CodeSplitterBuilder};
+pub use summarizer::{CodeSummarizer, CodeSummarizerBuilder};
 pub use supported_languages::SupportedLanguages;
