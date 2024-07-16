d1c642aa4ee9b373e395a78591dd36fa0379a4ff
Timon Vonk <timonv@gmail.com>
2024-07-16 12:21:30 +0200

    feat(groq): Add SimplePrompt support for Groq (#183)
    Adds simple prompt support for Groq by using async_openai. ~~Needs some
    double checks~~. Works great.

---
+++ swiftide/src/integrations/groq/config.rs
@@ -0,0 +1,55 @@
+use reqwest::header::{HeaderMap, AUTHORIZATION};
+use secrecy::{ExposeSecret as _, Secret};
+use serde::Deserialize;
+
+const GROQ_API_BASE: &str = "https://api.groq.com/openai/v1";
+
+#[derive(Clone, Debug, Deserialize)]
+#[serde(default)]
+pub struct GroqConfig {
+    api_base: String,
+    api_key: Secret<String>,
+}
+
+impl Default for GroqConfig {
+    fn default() -> Self {
+        Self {
+            api_base: GROQ_API_BASE.to_string(),
+            api_key: std::env::var("GROQ_API_KEY")
+                .unwrap_or_else(|_| String::new())
+                .into(),
+        }
+    }
+}
+
+impl async_openai::config::Config for GroqConfig {
+    fn headers(&self) -> HeaderMap {
+        let mut headers = HeaderMap::new();
+
+        headers.insert(
+            AUTHORIZATION,
+            format!("Bearer {}", self.api_key.expose_secret())
+                .as_str()
+                .parse()
+                .unwrap(),
+        );
+
+        headers
+    }
+
+    fn url(&self, path: &str) -> String {
+        format!("{}{}", self.api_base, path)
+    }
+
+    fn api_base(&self) -> &str {
+        &self.api_base
+    }
+
+    fn api_key(&self) -> &Secret<String> {
+        &self.api_key
+    }
+
+    fn query(&self) -> Vec<(&str, &str)> {
+        vec![]
+    }
+}
