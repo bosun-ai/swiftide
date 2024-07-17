5b848a3dcc7d930c233cb69c456ad3e6a8fca6ff
Timon Vonk <timonv@gmail.com>
2024-07-17 18:03:38 +0200

    Add feature flags

--- swiftide/src/transformers/mod.rs
+++ swiftide/src/transformers/mod.rs
@@ -9,23 +9,27 @@
 #[cfg(feature = "tree-sitter")]
 pub mod chunk_code;
 
+#[cfg(feature = "tree-sitter")]
+pub mod metadata_refs_defs_code;
+
+#[cfg(feature = "tree-sitter")]
+pub use chunk_code::ChunkCode;
+
+#[cfg(feature = "tree-sitter")]
+pub use metadata_refs_defs_code::MetadataRefsDefsCode;
+
 pub mod chunk_markdown;
 pub mod embed;
 pub mod metadata_keywords;
 pub mod metadata_qa_code;
 pub mod metadata_qa_text;
-pub mod metadata_refs_defs_code;
 pub mod metadata_summary;
 pub mod metadata_title;
-
-#[cfg(feature = "tree-sitter")]
-pub use chunk_code::ChunkCode;
 
 pub use chunk_markdown::ChunkMarkdown;
 pub use embed::Embed;
 pub use metadata_keywords::MetadataKeywords;
 pub use metadata_qa_code::MetadataQACode;
 pub use metadata_qa_text::MetadataQAText;
-pub use metadata_refs_defs_code::MetadataRefsDefsCode;
 pub use metadata_summary::MetadataSummary;
 pub use metadata_title::MetadataTitle;
