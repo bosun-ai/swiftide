f3d93138f78f1fea5ffba5588885d4643cb7e9b8
Timon Vonk <timonv@gmail.com>
2024-07-21 17:16:49 +0200

    Cleanup

--- swiftide-query/src/query/pipeline.rs
+++ swiftide-query/src/query/pipeline.rs
@@ -1,25 +1,14 @@
 use anyhow::Result;
 use futures_util::{StreamExt as _, TryStreamExt as _};
-use std::{
-    borrow::{Borrow, Cow},
-    sync::Arc,
-};
+use std::sync::Arc;
 use tokio::sync::mpsc::Sender;
 
 use tracing::Instrument as _;
 
 use crate::{
-    search_strategy::{self, SimilaritySingleEmbedding},
+    search_strategy::SimilaritySingleEmbedding,
     traits::{Answer, Retrieve, SearchStrategy, TransformQuery, TransformResponse},
 };
-
-// # Things to consider
-//
-// - For a simple flow this structure should work
-// - However, say we do subquestion generation, federate the queries, and then
-// want to rollup / rerank, a logical step might be generate more queries
-// - Similarly, if we do hybrid search or multi storage search, we might end up with multiple
-// datasets that would need to be filtered and consolidated
 
 use super::{
     query_stream::QueryStream,
@@ -27,9 +16,9 @@ use super::{
     Query,
 };
 
-pub struct Pipeline<'a, S: SearchStrategy = SimilaritySingleEmbedding, T = states::Pending> {
+pub struct Pipeline<'stream, S: SearchStrategy = SimilaritySingleEmbedding, T = states::Pending> {
     search_strategy: S,
-    stream: QueryStream<'a, T>,
+    stream: QueryStream<'stream, T>,
     query_sender: Sender<Result<Query<states::Pending>>>,
 }
 
@@ -37,7 +26,7 @@ impl Default for Pipeline<'_> {
     fn default() -> Self {
         let stream = QueryStream::default();
         Self {
-            search_strategy: Default::default(),
+            search_strategy: SimilaritySingleEmbedding::default(),
             query_sender: stream
                 .sender
                 .clone()
@@ -167,57 +156,28 @@ impl<'stream: 'static, S: SearchStrategy> Pipeline<'stream, S, states::Retrieved
 
 impl<'a, S: SearchStrategy> Pipeline<'a, S> {
     pub fn with_search_strategy(&mut self, strategy: S) -> &mut Pipeline<'a, S> {
-        self.search_strategy = strategy.into();
+        self.search_strategy = strategy;
 
         self
     }
+}
 
-    // pub fn then_retrieve<T: ToOwned<Owned = impl Retrieve<S> + 'static>>(
-    //     &mut self,
-    //     retriever: T,
-    // ) -> &mut Pipeline<S> {
-    //     let retriever = Arc::new(retriever.to_owned());
-    //     let stream = std::mem::take(&mut self.stream);
-    //
-    //     self.stream = stream
-    //         .map_ok(move |query| {
-    //             let transformer = Arc::clone(&retriever);
-    //             let span =
-    //                 tracing::trace_span!("then_retrieve", query = ?query, retriever = ?retriever);
-    //
-    //             async move { transformer.retrieve(query).await }.instrument(span)
-    //         })
-    //         .try_buffer_unordered(1)
-    //         .boxed()
-    //         .into();
-    //
-    //     self
-    // }
-
-    // pub fn then_transform_response<T: ToOwned<Owned = impl TransformResponse + 'static>>(
-    //     &mut self,
-    //     transformer: T,
-    // ) -> &mut Pipeline<S> {
-    //     let transformer = Arc::new(transformer.to_owned());
-    //     let stream = std::mem::take(&mut self.stream);
-    //
-    //     self.stream = stream
-    //         .map_ok(move |query| {
-    //             let transformer = Arc::clone(&transformer);
-    //             let span = tracing::trace_span!("then_transform_response", query = ?query, transformer = ?transformer);
-    //
-    //             async move { transformer.transform_response(query).await }.instrument(span)
-    //         })
-    //         .try_buffer_unordered(1)
-    //         .boxed()
-    //         .into();
-    //
-    //     self
-    // }
-
-    pub fn query(
-        &mut self,
+impl<S: SearchStrategy> Pipeline<'_, S, states::Answered> {
+    pub async fn query(
+        mut self,
         query: impl Into<Query<states::Pending>>,
     ) -> Result<Query<states::Answered>> {
+        self.query_sender.send(Ok(query.into())).await?;
+
+        let mut answer = None;
+        while let Some(first_answer) = self.stream.try_next().await? {
+            if answer.is_some() {
+                tracing::warn!("Received multiple answers, ignoring all but the first");
+                continue;
+            }
+            answer = Some(first_answer);
+        }
+
+        answer.ok_or_else(|| anyhow::anyhow!("No answer received"))
     }
 }
