9eb589416c2a56f9942b6f6bed3771cec6acebaf
Timon Vonk <timonv@gmail.com>
2024-08-02 19:34:48 +0200

    feat(query): add support for closures in all steps (#215)

--- swiftide-core/src/query_traits.rs
+++ swiftide-core/src/query_traits.rs
@@ -8,18 +8,31 @@ use crate::query::{
 
 /// Can transform queries before retrieval
 #[async_trait]
-pub trait TransformQuery: Send + Sync + std::fmt::Debug + ToOwned {
+pub trait TransformQuery: Send + Sync + ToOwned {
     async fn transform_query(
         &self,
         query: Query<states::Pending>,
     ) -> Result<Query<states::Pending>>;
 }
 
+#[async_trait]
+impl<F> TransformQuery for F
+where
+    F: Fn(Query<states::Pending>) -> Result<Query<states::Pending>> + Send + Sync + ToOwned,
+{
+    async fn transform_query(
+        &self,
+        query: Query<states::Pending>,
+    ) -> Result<Query<states::Pending>> {
+        (self)(query)
+    }
+}
+
 pub trait SearchStrategy: Clone + Send + Sync + Default {}
 
 /// Can retrieve documents given a SearchStrategy
 #[async_trait]
-pub trait Retrieve<S: SearchStrategy + ?Sized>: Send + Sync + std::fmt::Debug + ToOwned {
+pub trait Retrieve<S: SearchStrategy + ?Sized>: Send + Sync + ToOwned {
     async fn retrieve(
         &self,
         search_strategy: &S,
@@ -27,15 +40,50 @@ pub trait Retrieve<S: SearchStrategy + ?Sized>: Send + Sync + std::fmt::Debug + 
     ) -> Result<Query<states::Retrieved>>;
 }
 
+#[async_trait]
+impl<S, F> Retrieve<S> for F
+where
+    S: SearchStrategy + ?Sized,
+    F: Fn(&S, Query<states::Pending>) -> Result<Query<states::Retrieved>> + Send + Sync + ToOwned,
+{
+    async fn retrieve(
+        &self,
+        search_strategy: &S,
+        query: Query<states::Pending>,
+    ) -> Result<Query<states::Retrieved>> {
+        (self)(search_strategy, query)
+    }
+}
+
 /// Can transform a response after retrieval
 #[async_trait]
-pub trait TransformResponse: Send + Sync + std::fmt::Debug + ToOwned {
+pub trait TransformResponse: Send + Sync + ToOwned {
     async fn transform_response(&self, query: Query<Retrieved>)
         -> Result<Query<states::Retrieved>>;
 }
 
+#[async_trait]
+impl<F> TransformResponse for F
+where
+    F: Fn(Query<Retrieved>) -> Result<Query<Retrieved>> + Send + Sync + ToOwned,
+{
+    async fn transform_response(&self, query: Query<Retrieved>) -> Result<Query<Retrieved>> {
+        (self)(query)
+    }
+}
+
 /// Can answer the original query
 #[async_trait]
-pub trait Answer: Send + Sync + std::fmt::Debug + ToOwned {
+pub trait Answer: Send + Sync + ToOwned {
     async fn answer(&self, query: Query<states::Retrieved>) -> Result<Query<states::Answered>>;
+}
+
+#[async_trait]
+impl<F> Answer for F
+where
+    F: Fn(Query<Retrieved>) -> Result<Query<states::Answered>> + Send + Sync + ToOwned,
+{
+    async fn answer(&self, query: Query<Retrieved>) -> Result<Query<states::Answered>> {
+        (self)(query)
+    }
 }
