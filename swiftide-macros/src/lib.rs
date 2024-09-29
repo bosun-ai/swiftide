1b0e691e39d3e7e0f91410f11fab1b986d148873
Timon Vonk <timonv@gmail.com>
2024-09-29 16:52:02 +0200

    docs(swiftide-macros): Preserve doc comments on indexing transformer (#342)

--- swiftide-macros/src/lib.rs
+++ swiftide-macros/src/lib.rs
@@ -4,9 +4,11 @@ use proc_macro::TokenStream;
 
 mod indexing_transformer;
 use indexing_transformer::indexing_transformer_impl;
+use syn::{parse_macro_input, ItemStruct};
 
 /// Generates boilerplate for an indexing transformer.
 #[proc_macro_attribute]
-pub fn indexing_transformer(args: TokenStream, item: TokenStream) -> TokenStream {
-    indexing_transformer_impl(args, item)
+pub fn indexing_transformer(args: TokenStream, input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as ItemStruct);
+    indexing_transformer_impl(args.into(), input).into()
 }
