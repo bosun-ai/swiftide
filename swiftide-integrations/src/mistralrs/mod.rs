cdd96828cec66d8a597ff16f8aa325d2b6cc0611
Timon Vonk <timonv@gmail.com>
2024-10-27 22:09:27 +0100

    fix(integrations): Revert mistralrs support (#418)
    Unfortunately we cannot publish unpublished crates. When mistral-rs is
    published, we are happy to add support again.
    
    This reverts commit a866d38b2cbaf7154ace957472c75b99f1c9c070.

--- swiftide-integrations/src/mistralrs/mod.rs
+++
@@ -1,9 +0,0 @@
-//! Use mistral.rs right inside your pipeline
-//!
-//! Mistral.rs supports a variety of hugging face models.
-//!
-//! See [the mistral.rs github page](https://github.com/EricLBuehler/mistral.rs/) for more
-//! information.
-mod simple_prompt;
-
-pub use simple_prompt::{Mistralrs, MistralrsBuilder};
