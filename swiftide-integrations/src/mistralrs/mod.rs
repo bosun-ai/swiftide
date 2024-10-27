a866d38b2cbaf7154ace957472c75b99f1c9c070
Timon Vonk <timonv@gmail.com>
2024-10-27 16:43:30 +0100

    feat(integrations): Support in process hugging face models via mistralrs (#386)

---
+++ swiftide-integrations/src/mistralrs/mod.rs
@@ -0,0 +1,9 @@
+//! Use mistral.rs right inside your pipeline
+//!
+//! Mistral.rs supports a variety of hugging face models.
+//!
+//! See [the mistral.rs github page](https://github.com/EricLBuehler/mistral.rs/) for more
+//! information.
+mod simple_prompt;
+
+pub use simple_prompt::{Mistralrs, MistralrsBuilder};
