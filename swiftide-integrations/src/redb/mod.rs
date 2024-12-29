55c27bdd90c05c544aaa2e60455d468dfe086cd5
Timon Vonk <mail@timonv.nl>
2024-12-29 19:31:14 +0100

    feat(redb): Public database and table definition

--- swiftide-integrations/src/redb/mod.rs
+++ swiftide-integrations/src/redb/mod.rs
@@ -77,8 +77,11 @@ impl Redb {
         format!("{}.{}", self.cache_key_prefix, node.id())
     }
 
-    fn table_definition(&self) -> redb::TableDefinition<String, bool> {
-        // TODO: Should this be static?
+    pub fn table_definition(&self) -> redb::TableDefinition<String, bool> {
         redb::TableDefinition::<String, bool>::new(&self.table_name)
+    }
+
+    pub fn database(&self) -> &redb::Database {
+        &self.database
     }
 }
