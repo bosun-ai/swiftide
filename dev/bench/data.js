add79420b483b4102b668caa462281d025292228
github-action-benchmark <github@users.noreply.github.com>
2025-01-10 13:42:32 +0000

    add Rust Benchmark (cargo) benchmark result for b66bd79070772d7e1bfe10a22531ccfd6501fc2a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1736413238554,
+  "lastUpdate": 1736516552643,
   "repoUrl": "https://github.com/bosun-ai/swiftide",
   "entries": {
     "Rust Benchmark": [
@@ -12095,6 +12095,60 @@ window.BENCHMARK_DATA = {
             "name": "node_cache/redb",
             "value": 244758,
             "range": "± 1266",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "timonv@gmail.com",
+            "name": "Timon Vonk",
+            "username": "timonv"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b66bd79070772d7e1bfe10a22531ccfd6501fc2a",
+          "message": "feat(fastembed): Add support for jina v2 code (#541)\n\nAdd support for jina v2 code in fastembed.\r\n\r\nResolves #537",
+          "timestamp": "2025-01-10T14:33:40+01:00",
+          "tree_id": "ffff84415d2c90a7e25d3ed5d13496244b7a83ff",
+          "url": "https://github.com/bosun-ai/swiftide/commit/b66bd79070772d7e1bfe10a22531ccfd6501fc2a"
+        },
+        "date": 1736516551812,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "load_1",
+            "value": 7,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "load_10",
+            "value": 7,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "run_local_pipeline",
+            "value": 176,
+            "range": "± 3",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "node_cache/redis",
+            "value": 1361895,
+            "range": "± 1032719",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "node_cache/redb",
+            "value": 243896,
+            "range": "± 1998",
             "unit": "ns/iter"
           }
         ]
