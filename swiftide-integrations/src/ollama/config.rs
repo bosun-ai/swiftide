a9488d24afbbd2f6759e6802f85147c19ea71c08
Timon Vonk <mail@timonv.nl>
2025-01-11 13:38:39 +0100

    Copy pasta from async openai

---
+++ swiftide-integrations/src/ollama/config.rs
@@ -0,0 +1,43 @@
+use reqwest::header::{HeaderMap, AUTHORIZATION};
+use secrecy::{ExposeSecret as _, Secret};
+use serde::Deserialize;
+
+const OLLAMA_API_BASE: &str = "http://localhost:11434";
+
+#[derive(Clone, Debug, Deserialize)]
+#[serde(default)]
+pub struct OllamaConfig {
+    api_base: String,
+    api_key: Secret<String>,
+}
+
+impl Default for OllamaConfig {
+    fn default() -> Self {
+        Self {
+            api_base: OLLAMA_API_BASE.to_string(),
+            api_key: String::new().into(),
+        }
+    }
+}
+
+impl async_openai::config::Config for OllamaConfig {
+    fn headers(&self) -> HeaderMap {
+        HeaderMap::new()
+    }
+
+    fn url(&self, path: &str) -> String {
+        format!("{}{}", self.api_base, path)
+    }
+
+    fn api_base(&self) -> &str {
+        &self.api_base
+    }
+
+    fn api_key(&self) -> &Secret<String> {
+        &self.api_key
+    }
+
+    fn query(&self) -> Vec<(&str, &str)> {
+        vec![]
+    }
+}
