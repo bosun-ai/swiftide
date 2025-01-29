99fe3461b76417139a45b555e22de66d353e6e5e
Timon Vonk <mail@timonv.nl>
2025-01-29 18:21:40 +0100

    At least it compiles

--- swiftide-integrations/src/duckdb/upsert.sql
+++ swiftide-integrations/src/duckdb/upsert.sql
@@ -1,15 +1,13 @@
-INSERT INTO {{ table_name }} (uuid, chunk, path, metadata, original_size, offset, {{ vector_field_names | join(",") }})
-VALUES (?, ?, ?, ?, ?, ?, 
-  {% for _ in range(end=vector_field_names | len) %}
+INSERT INTO {{ table_name }} (uuid, chunk, path, metadata, {{ vector_field_names | join(sep=", ") }})
+VALUES (?, ?, ?, ?,
+  {% for _ in range(end=vector_field_names | length) %}
     ?,
   {% endfor %}
   )
-) ON CONFINCT (uuid) DO UPDATE SET
+ON CONFLICT (uuid) DO UPDATE SET
   chunk = EXCLUDED.chunk,
   path = EXCLUDED.path,
   metadata = EXCLUDED.metadata,
-  original_size = EXCLUDED.original_size,
-  offset = EXCLUDED.offset,
   {% for vector in vector_field_names %}
     {{ vector }} = EXCLUDED.{{ vector }},
   {% endfor %}
