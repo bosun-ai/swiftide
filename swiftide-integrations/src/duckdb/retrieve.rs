7179bde962d0ebbed20ad4832175c63d798e9fb7
Timon Vonk <mail@timonv.nl>
2025-02-12 17:16:59 +0100

    wip

--- swiftide-integrations/src/duckdb/retrieve.rs
+++ swiftide-integrations/src/duckdb/retrieve.rs
@@ -1,4 +1,4 @@
 use duckdb::Statement;
 use swiftide_core::{querying::search_strategies::CustomStrategy, Retrieve};
 
-impl Retrieve<CustomStrategy<Statement<''>
+// impl Retrieve<CustomStrategy<Statement<''>
