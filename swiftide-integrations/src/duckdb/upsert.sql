a167c72a80e9dd5215254481852bb50a290292f5
Timon Vonk <mail@timonv.nl>
2025-03-09 11:57:07 +0100

    fix: Disable vector upsert on duckdb

--- swiftide-integrations/src/duckdb/upsert.sql
+++ swiftide-integrations/src/duckdb/upsert.sql
@@ -7,7 +7,8 @@ VALUES (?, ?, ?,
 ON CONFLICT (uuid) DO UPDATE SET
   chunk = EXCLUDED.chunk,
   path = EXCLUDED.path,
-  {% for vector in vector_field_names %}
-    {{ vector }} = EXCLUDED.{{ vector }},
-  {% endfor %}
-  ;
+-- We cannot do true upserts in 1.1.1. This is supported in 1.2.0
+  -- {% for vector in vector_field_names %}
+  --   {{ vector }} = EXCLUDED.{{ vector }},
+  -- {% endfor %}
+;
