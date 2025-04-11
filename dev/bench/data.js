36f6a1b80062ecc3d17d0c61790f347954b0bda8
github-action-benchmark <github@users.noreply.github.com>
2025-04-11 10:18:53 +0000

    add Rust Benchmark (cargo) benchmark result for bcb10d1d040b7bce70138854703605cae8e991b9

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1744366719428,
+  "lastUpdate": 1744366733365,
   "repoUrl": "https://github.com/bosun-ai/swiftide",
   "entries": {
     "Rust Benchmark": [
@@ -19589,6 +19589,60 @@ window.BENCHMARK_DATA = {
             "name": "node_cache/redb",
             "value": 247410,
             "range": "± 2034",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "bcb10d1d040b7bce70138854703605cae8e991b9",
+          "message": "chore(deps): bump crossbeam-channel from 0.5.14 to 0.5.15 in the cargo group (#733)\n\nBumps the cargo group with 1 update:\n[crossbeam-channel](https://github.com/crossbeam-rs/crossbeam).\n\nUpdates `crossbeam-channel` from 0.5.14 to 0.5.15\n<details>\n<summary>Release notes</summary>\n<p><em>Sourced from <a\nhref=\"https://github.com/crossbeam-rs/crossbeam/releases\">crossbeam-channel's\nreleases</a>.</em></p>\n<blockquote>\n<h2>crossbeam-channel 0.5.15</h2>\n<ul>\n<li>Fix regression introduced in 0.5.12 that can lead to a double free\nwhen dropping unbounded channel. (<a\nhref=\"https://redirect.github.com/crossbeam-rs/crossbeam/issues/1187\">#1187</a>)</li>\n</ul>\n</blockquote>\n</details>\n<details>\n<summary>Commits</summary>\n<ul>\n<li><a\nhref=\"https://github.com/crossbeam-rs/crossbeam/commit/d35ffde18a76ae9667f602d8c0127ff89ac2e4d4\"><code>d35ffde</code></a>\nPrepare for the next release</li>\n<li><a\nhref=\"https://github.com/crossbeam-rs/crossbeam/commit/6ec74ecae896df5fc239518b45a1bfd258c9db68\"><code>6ec74ec</code></a>\ncrossbeam-channel: prevent double free on Drop (<a\nhref=\"https://redirect.github.com/crossbeam-rs/crossbeam/issues/1187\">#1187</a>)</li>\n<li>See full diff in <a\nhref=\"https://github.com/crossbeam-rs/crossbeam/compare/crossbeam-channel-0.5.14...crossbeam-channel-0.5.15\">compare\nview</a></li>\n</ul>\n</details>\n<br />\n\n\n[![Dependabot compatibility\nscore](https://dependabot-badges.githubapp.com/badges/compatibility_score?dependency-name=crossbeam-channel&package-manager=cargo&previous-version=0.5.14&new-version=0.5.15)](https://docs.github.com/en/github/managing-security-vulnerabilities/about-dependabot-security-updates#about-compatibility-scores)\n\nDependabot will resolve any conflicts with this PR as long as you don't\nalter it yourself. You can also trigger a rebase manually by commenting\n`@dependabot rebase`.\n\n[//]: # (dependabot-automerge-start)\n[//]: # (dependabot-automerge-end)\n\n---\n\n<details>\n<summary>Dependabot commands and options</summary>\n<br />\n\nYou can trigger Dependabot actions by commenting on this PR:\n- `@dependabot rebase` will rebase this PR\n- `@dependabot recreate` will recreate this PR, overwriting any edits\nthat have been made to it\n- `@dependabot merge` will merge this PR after your CI passes on it\n- `@dependabot squash and merge` will squash and merge this PR after\nyour CI passes on it\n- `@dependabot cancel merge` will cancel a previously requested merge\nand block automerging\n- `@dependabot reopen` will reopen this PR if it is closed\n- `@dependabot close` will close this PR and stop Dependabot recreating\nit. You can achieve the same result by closing it manually\n- `@dependabot show <dependency name> ignore conditions` will show all\nof the ignore conditions of the specified dependency\n- `@dependabot ignore <dependency name> major version` will close this\ngroup update PR and stop Dependabot creating any more for the specific\ndependency's major version (unless you unignore this specific\ndependency's major version or upgrade to it yourself)\n- `@dependabot ignore <dependency name> minor version` will close this\ngroup update PR and stop Dependabot creating any more for the specific\ndependency's minor version (unless you unignore this specific\ndependency's minor version or upgrade to it yourself)\n- `@dependabot ignore <dependency name>` will close this group update PR\nand stop Dependabot creating any more for the specific dependency\n(unless you unignore this specific dependency or upgrade to it yourself)\n- `@dependabot unignore <dependency name>` will remove all of the ignore\nconditions of the specified dependency\n- `@dependabot unignore <dependency name> <ignore condition>` will\nremove the ignore condition of the specified dependency and ignore\nconditions\nYou can disable automated security fix PRs for this repo from the\n[Security Alerts\npage](https://github.com/bosun-ai/swiftide/network/alerts).\n\n</details>\n\nSigned-off-by: dependabot[bot] <support@github.com>\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2025-04-11T12:10:27+02:00",
+          "tree_id": "66d7056d1a89448df266f90ab49839110833c434",
+          "url": "https://github.com/bosun-ai/swiftide/commit/bcb10d1d040b7bce70138854703605cae8e991b9"
+        },
+        "date": 1744366732599,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "load_1",
+            "value": 7,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "load_10",
+            "value": 7,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "run_local_pipeline",
+            "value": 176,
+            "range": "± 4",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "node_cache/redis",
+            "value": 1287198,
+            "range": "± 980148",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "node_cache/redb",
+            "value": 249031,
+            "range": "± 1658",
             "unit": "ns/iter"
           }
         ]
