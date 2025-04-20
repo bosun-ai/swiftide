38edbfd51811f4cf7bf4732e5a8374f160ffa662
github-action-benchmark <github@users.noreply.github.com>
2025-04-20 13:04:40 +0000

    add Rust Benchmark (cargo) benchmark result for 6635973bea22dd96ac90285815ecce295f575e80

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1744899148486,
+  "lastUpdate": 1745154280320,
   "repoUrl": "https://github.com/bosun-ai/swiftide",
   "entries": {
     "Rust Benchmark": [
@@ -20345,6 +20345,60 @@ window.BENCHMARK_DATA = {
             "name": "node_cache/redb",
             "value": 249951,
             "range": "± 1433",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6635973bea22dd96ac90285815ecce295f575e80",
+          "message": "chore(deps): bump spider from 2.36.59 to 2.36.63 in the minor group (#751)\n\nBumps the minor group with 1 update:\n[spider](https://github.com/spider-rs/spider).\n\nUpdates `spider` from 2.36.59 to 2.36.63\n<details>\n<summary>Release notes</summary>\n<p><em>Sourced from <a\nhref=\"https://github.com/spider-rs/spider/releases\">spider's\nreleases</a>.</em></p>\n<blockquote>\n<h2>v2.36.63</h2>\n<h1>Whats Changed</h1>\n<p>Fix xml parsing initial links.</p>\n<ul>\n<li>chore(real_browser,chrome): add missing chrome headers</li>\n<li>chore(chrome): add real browser loading</li>\n<li>chore(chrome): fix request_timeout default</li>\n<li>chore(chrome): fix timeout subtracting</li>\n</ul>\n<p><strong>Full Changelog</strong>: <a\nhref=\"https://github.com/spider-rs/spider/compare/v2.35.18...v2.36.63\">https://github.com/spider-rs/spider/compare/v2.35.18...v2.36.63</a></p>\n</blockquote>\n</details>\n<details>\n<summary>Commits</summary>\n<ul>\n<li>See full diff in <a\nhref=\"https://github.com/spider-rs/spider/commits/v2.36.63\">compare\nview</a></li>\n</ul>\n</details>\n<br />\n\n\n[![Dependabot compatibility\nscore](https://dependabot-badges.githubapp.com/badges/compatibility_score?dependency-name=spider&package-manager=cargo&previous-version=2.36.59&new-version=2.36.63)](https://docs.github.com/en/github/managing-security-vulnerabilities/about-dependabot-security-updates#about-compatibility-scores)\n\nDependabot will resolve any conflicts with this PR as long as you don't\nalter it yourself. You can also trigger a rebase manually by commenting\n`@dependabot rebase`.\n\n[//]: # (dependabot-automerge-start)\n[//]: # (dependabot-automerge-end)\n\n---\n\n<details>\n<summary>Dependabot commands and options</summary>\n<br />\n\nYou can trigger Dependabot actions by commenting on this PR:\n- `@dependabot rebase` will rebase this PR\n- `@dependabot recreate` will recreate this PR, overwriting any edits\nthat have been made to it\n- `@dependabot merge` will merge this PR after your CI passes on it\n- `@dependabot squash and merge` will squash and merge this PR after\nyour CI passes on it\n- `@dependabot cancel merge` will cancel a previously requested merge\nand block automerging\n- `@dependabot reopen` will reopen this PR if it is closed\n- `@dependabot close` will close this PR and stop Dependabot recreating\nit. You can achieve the same result by closing it manually\n- `@dependabot show <dependency name> ignore conditions` will show all\nof the ignore conditions of the specified dependency\n- `@dependabot ignore <dependency name> major version` will close this\ngroup update PR and stop Dependabot creating any more for the specific\ndependency's major version (unless you unignore this specific\ndependency's major version or upgrade to it yourself)\n- `@dependabot ignore <dependency name> minor version` will close this\ngroup update PR and stop Dependabot creating any more for the specific\ndependency's minor version (unless you unignore this specific\ndependency's minor version or upgrade to it yourself)\n- `@dependabot ignore <dependency name>` will close this group update PR\nand stop Dependabot creating any more for the specific dependency\n(unless you unignore this specific dependency or upgrade to it yourself)\n- `@dependabot unignore <dependency name>` will remove all of the ignore\nconditions of the specified dependency\n- `@dependabot unignore <dependency name> <ignore condition>` will\nremove the ignore condition of the specified dependency and ignore\nconditions\n\n\n</details>\n\nSigned-off-by: dependabot[bot] <support@github.com>\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2025-04-20T14:56:03+02:00",
+          "tree_id": "2769a43a098c4ea402a6d8ffbf90ff102781aa7a",
+          "url": "https://github.com/bosun-ai/swiftide/commit/6635973bea22dd96ac90285815ecce295f575e80"
+        },
+        "date": 1745154279324,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "load_1",
+            "value": 7,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "load_10",
+            "value": 7,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "run_local_pipeline",
+            "value": 176,
+            "range": "± 4",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "node_cache/redis",
+            "value": 1291280,
+            "range": "± 931183",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "node_cache/redb",
+            "value": 254851,
+            "range": "± 1880",
             "unit": "ns/iter"
           }
         ]
