1e78244d923796c984975c492ff44c51dcb7711b
Timon Vonk <mail@timonv.nl>
2025-04-25 13:07:42 +0200

    pub from root

--- swiftide-agents/src/lib.rs
+++ swiftide-agents/src/lib.rs
@@ -61,7 +61,7 @@ mod util;
 
 pub use agent::{Agent, AgentBuilder, AgentBuilderError};
 pub use default_context::DefaultContext;
-pub use tasks::{Task, TaskBuilder, TaskBuilderError, TaskError};
+pub use tasks::*;
 
 #[cfg(any(test, debug_assertions))]
 pub mod test_utils;
