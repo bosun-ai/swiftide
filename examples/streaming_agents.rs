2dc64952997cf88e90b91702de6a3f02427a6d9b
Timon Vonk <mail@timonv.nl>
2025-06-08 11:16:38 +0200

    Update example

--- examples/streaming_agents.rs
+++ examples/streaming_agents.rs
@@ -1,20 +1,25 @@
 //! This example demonstrates how to stream responses from an agent
+//!
+//! By default, for convenience the accumulated response is streamed. You can opt-out of this
+//! behaviour and only receive the delta as well (only with OpenAI-like providers).
 use anyhow::Result;
 use swiftide::agents;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    // let openai = swiftide::integrations::openai::OpenAI::builder()
-    //     .default_embed_model("text-embeddings-3-small")
-    //     .default_prompt_model("gpt-4o-mini")
-    //     .build()?;
-    //
-    let anthropic = swiftide::integrations::anthropic::Anthropic::builder()
-        .default_prompt_model("claude-3-7-sonnet-latest")
+    let openai = swiftide::integrations::openai::OpenAI::builder()
+        .default_embed_model("text-embeddings-3-small")
+        .default_prompt_model("gpt-4o-mini")
+        // Only streams the delta, leave this out to stream the full response
+        .stream_full(false)
         .build()?;
 
+    // let anthropic = swiftide::integrations::anthropic::Anthropic::builder()
+    //     .default_prompt_model("claude-3-7-sonnet-latest")
+    //     .build()?;
+
     agents::Agent::builder()
-        .llm(&anthropic)
+        .llm(&openai)
         .on_stream(|_agent, response| {
             // We print the message chunk if it exists. Streamed responses also include
             // the full response (without tool calls) in `message` and an `id` to map them to
@@ -31,6 +36,8 @@ async fn main() -> Result<()> {
                         .unwrap_or_default()
                 );
             };
+            // If `stream_full` is disabled, response.message() will be the accumulated response
+            // response.message()
 
             Box::pin(async move { Ok(()) })
         })
