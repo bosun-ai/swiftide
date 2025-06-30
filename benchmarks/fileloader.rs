208d836ce7a397b74c5359cd529f36ca4aaf0f8d
Timon Vonk <mail@timonv.nl>
2025-06-30 11:06:21 +0200

    Clippy

--- benchmarks/fileloader.rs
+++ benchmarks/fileloader.rs
@@ -1,5 +1,7 @@
+use std::hint::black_box;
+
 use anyhow::Result;
-use criterion::{Criterion, black_box, criterion_group, criterion_main};
+use criterion::{Criterion, criterion_group, criterion_main};
 use futures_util::stream::{StreamExt, TryStreamExt};
 use swiftide::traits::Loader;
 
