c1a0395a3ff00b7da994734fcb8d7357bf4c2199
Timon Vonk <mail@timonv.nl>
2025-07-22 15:35:31 +0200

    feat(agents): Multi-agent tasks with graphs
    Refactor into separate files
    
    more wip
    
    Clean up errors
    
    Rename to tasks
    
    Support closures and all the generics
    
    Clippy
    
    Add typed node id to TaskNode#evaluate
    
    Use type erased node id for node trait
    
    Use dyn for trait
    
    Remove clone requirements
    
    Try borrowed
    
    Remove sized requirements
    
    Implement Clone for tasks
    
    Rename stuff
    
    Make it properly work with dyn NodeId
    
    Add reset and NoSteps error
    
    Clippy
    
    Clippy
    
    Add basic tests and very current transition and node work
    
    Do not reset any existing context
    
    Do not consume the current context
    
    Implement for box dyn task node

---
+++ swiftide-agents/src/tasks/mod.rs
@@ -0,0 +1,6 @@
+pub mod closures;
+pub mod errors;
+pub mod impls;
+pub mod node;
+pub mod task;
+pub mod transition;
---
+++ swiftide-agents/src/tasks/transition.rs
@@ -0,0 +1,170 @@
+use std::{any::Any, sync::Arc};
+
+use async_trait::async_trait;
+use dyn_clone::DynClone;
+
+use super::{
+    errors::NodeError,
+    node::{NodeArg, NodeId, TaskNode},
+};
+
+pub trait TransitionFn:
+    for<'a> Fn(Box<Self::Input>) -> TransitionPayload + Send + Sync + DynClone
+{
+    type Input: NodeArg + 'static;
+}
+
+// impl<F, I> TransitionFn for F
+// where
+//     F: for<'a> Fn(&'a Self::Input) -> TransitionPayload + Send + Sync + DynClone,
+//     I: NodeArg + 'static,
+// {
+//     type Input = I;
+// }
+
+pub(crate) struct Transition<
+    Input: NodeArg,
+    Output: NodeArg,
+    Error: std::error::Error + Send + Sync + 'static,
+> {
+    pub(crate) node: Box<dyn TaskNode<Input = Input, Output = Output, Error = Error> + Send + Sync>,
+    pub(crate) node_id: NodeId<dyn TaskNode<Input = Input, Output = Output, Error = Error>>,
+    pub(crate) r#fn: Arc<dyn Fn(Output) -> TransitionPayload + Send + Sync>,
+    // pub(crate) r#fn: Box<dyn TransitionFn<Input = T::Input> + Send + Sync>,
+    pub(crate) is_set: bool,
+}
+
+impl<Input, Output, Error> Clone for Transition<Input, Output, Error>
+where
+    Input: NodeArg,
+    Output: NodeArg,
+    Error: std::error::Error + Send + Sync + 'static,
+{
+    fn clone(&self) -> Self {
+        Transition {
+            node: self.node.clone(),
+            node_id: self.node_id,
+            r#fn: self.r#fn.clone(),
+            is_set: self.is_set,
+        }
+    }
+}
+
+impl<Input: NodeArg, Output: NodeArg, Error: std::error::Error + Send + Sync + 'static>
+    std::fmt::Debug for Transition<Input, Output, Error>
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Transition")
+            .field("node_id", &self.node_id)
+            .field("is_set", &self.is_set)
+            .finish()
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct NextNode {
+    // If we make this an enum instead, we can support spawning many nodes as well
+    pub(crate) node_id: usize,
+    pub(crate) context: Arc<dyn Any + Send + Sync>,
+}
+
+impl NextNode {
+    pub fn new<T: TaskNode + ?Sized>(node_id: NodeId<T>, context: T::Input) -> Self
+    where
+        <T as TaskNode>::Input: 'static,
+    {
+        let context = Arc::new(context) as Arc<dyn Any + Send + Sync>;
+
+        NextNode {
+            node_id: node_id.id,
+            context,
+        }
+    }
+}
+
+impl From<NextNode> for TransitionPayload {
+    fn from(next_node: NextNode) -> Self {
+        TransitionPayload::NextNode(next_node)
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum TransitionPayload {
+    NextNode(NextNode),
+    Pause,
+}
+
+impl TransitionPayload {
+    pub fn next_node<T: TaskNode + 'static + ?Sized>(
+        node_id: &NodeId<T>,
+        context: T::Input,
+    ) -> Self {
+        NextNode::new(*node_id, context).into()
+    }
+
+    pub fn pause() -> Self {
+        TransitionPayload::Pause
+    }
+}
+
+pub struct MarkedTransitionPayload<To: TaskNode + ?Sized>(
+    TransitionPayload,
+    std::marker::PhantomData<To>,
+);
+
+impl<To: TaskNode + ?Sized> MarkedTransitionPayload<To> {
+    pub fn new(payload: TransitionPayload) -> Self {
+        MarkedTransitionPayload(payload, std::marker::PhantomData)
+    }
+
+    pub fn into_inner(self) -> TransitionPayload {
+        self.0
+    }
+}
+
+impl<T: TaskNode> std::ops::Deref for MarkedTransitionPayload<T> {
+    type Target = TransitionPayload;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+#[async_trait]
+pub(crate) trait AnyNodeTransition: Any + Send + Sync + std::fmt::Debug + DynClone {
+    fn transition_is_set(&self) -> bool;
+
+    async fn evaluate_next(
+        &self,
+        context: Arc<dyn Any + Send + Sync>,
+    ) -> Result<TransitionPayload, NodeError>;
+
+    fn node_id(&self) -> usize;
+}
+
+dyn_clone::clone_trait_object!(AnyNodeTransition);
+
+#[async_trait]
+impl<Input: NodeArg, Output: NodeArg, Error: std::error::Error + Send + Sync + 'static>
+    AnyNodeTransition for Transition<Input, Output, Error>
+{
+    async fn evaluate_next(
+        &self,
+        context: Arc<dyn Any + Send + Sync>,
+    ) -> Result<TransitionPayload, NodeError> {
+        let context = context.downcast::<Input>().unwrap();
+
+        match self.node.evaluate(&self.node_id.as_dyn(), &context).await {
+            Ok(output) => Ok((self.r#fn)(output)),
+            Err(error) => Err(NodeError::new(error, self.node_id.id, None)), // node_id will be set by caller
+        }
+    }
+
+    fn transition_is_set(&self) -> bool {
+        self.is_set
+    }
+
+    fn node_id(&self) -> usize {
+        self.node_id.id
+    }
+}
